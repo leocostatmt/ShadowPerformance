4f3192d0c41c7160e61597d87e9f2bd15a3f0cd5
leocostatmt <leonardocaetanocosta2005@gmail.com>
2026-06-23 18:22:39 -0300

    Changes to be committed: 	modified:   src/main/java/com/cursodevjava/javaproject/Usuario.java

--- src/main/java/com/cursodevjava/javaproject/Usuario.java
+++ src/main/java/com/cursodevjava/javaproject/Usuario.java
@@ -20,18 +20,30 @@ public class Usuario {
     private LocalDateTime tokenExpiracao;
 
     // Getters e Setters
-    public Long getId() { return id; }
+    public Long getId() { 
+        return id; 
+    }
     public void setId(Long id) { this.id = id; }
 
-    public String getNome() { return nome; }
+
+    public String getNome() { 
+        return nome; 
+    }
     public void setNome(String nome) { this.nome = nome; }
 
-    public String getEmail() { return email; }
+
+    public String getEmail() { 
+        return email; 
+    }
     public void setEmail(String email) { this.email = email; }
 
-    public String getSenha() { return senha; }
+
+    public String getSenha() { 
+        return senha; 
+    }
     public void setSenha(String senha) { this.senha = senha; }
 
+    
     public String getTokenRecuperacao() {
     return tokenRecuperacao;
 }
